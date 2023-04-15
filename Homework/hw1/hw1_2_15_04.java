3943734e7baf29c348d2dd6ecaeb1ed03ed2d035
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2023-04-15 23:14:22 +0300

    Finished: hw1_2

--- Homework/hw1/hw1_2_15_04.java
+++ Homework/hw1/hw1_2_15_04.java
@@ -12,22 +12,31 @@ import java.util.Scanner;
 
 public class hw1_2_15_04 {
     public static void main (String[] args) {
-        System.out.println("Введите число и мы найдем все простые числа до него: ");
-        Scanner cr = new Scanner(System.in);
-        
-
+        int user_number = getUserNumber();
+        List<Integer> primes_answer = getPrimeNumbers(user_number);
+        System.out.println(primes_answer);
     }
 
-    public List<Integer> getPrimeNumbers(int arrSize){
-        var arrBool = new boolean[arrSize];
+    public static int getUserNumber(){
+        System.out.println("Введите число и мы найдем все простые числа до него: ");
+        Scanner sc = new Scanner(System.in);
+        int number = sc.nextInt();
+        sc.close();
+        return number;
+    }
+
+    public static List<Integer> getPrimeNumbers(int arrSize){
+        boolean[] arrBool = new boolean[arrSize];
         Arrays.fill(arrBool, true);
         arrBool[0] = false;
         arrBool[1] = false;
 
         for (int i = 0; i < arrBool.length; i++) {
             if (arrBool[i]) {
-                for (int j = 0; j < arrBool.length; j++) {
+                for (int j = 2; i * j < arrBool.length; j++ ) {
+                    // System.out.printf("i = %d, j = d%, i * j = d%", i * j);
                     arrBool[i * j] = false;
+
                 }
             }
         }
