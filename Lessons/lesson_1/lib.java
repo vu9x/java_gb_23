117b9ea6ffa2d2de5b7c277a89c47c3d73f1b750
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2023-04-15 13:29:29 +0300

    Lesson 1

---
+++ Lessons/lesson_1/lib.java
@@ -0,0 +1,6 @@
+package java_gb_23.Lessons.lesson_1;
+public class lib {
+    public static void sayHi() {
+        System.out.println("Hi!");
+    }
+}
---
+++ Lessons/lesson_1/program.java
@@ -0,0 +1,34 @@
+package java_gb_23.Lessons.lesson_1;
+
+/**
+ * program
+ */
+public class program {
+    // public static void main(String[] args) {
+    //     System.out.println("world12312"); // new comments
+    //     // float number = 123.45f;
+    // }
+
+    // public static void main(String[] args) {
+    //     int [][] arr = new int[3][5] ;;
+
+    //     for (int i = 0; i < arr.length; i++) {
+    //         for (int j = 0; j < arr[i].length; j++) {
+    //             arr[i][j] = j;
+    //             System.out.printf("%d ", arr[i][j]);
+    //         }
+    //     System.out.println();
+    //     }
+    // }
+//     public static void main(String[] args) {
+        
+//         int a = 1, b = 2;
+//         int c = a + b;
+//         String res = a + " + " + b + " = " + c;
+//         System.out.println(res);
+//     }
+
+    public static void main(String[] args) {
+        lib.sayHi();
+}
+}
