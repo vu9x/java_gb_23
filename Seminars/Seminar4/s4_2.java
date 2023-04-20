6433f83541f5aaa8bcb8b92af874ad145bdb0bee
Vu_Nguyen <nguyentuanvu9x@gmail.com>
2023-04-20 16:55:29 +0300

    Added: s4_2.java

--- Seminars/Seminar4/s4_2.java
+++ Seminars/Seminar4/s4_2.java
@@ -1,7 +1,30 @@
 package java_gb_23.Seminars.Seminar4;
 
+import java.util.LinkedList;
+import java.util.Scanner;
+
 public class s4_2 {
     public static void main(String[] args) {
+        LinkedList<String> list = new LinkedList<String>();
+        Scanner sc = new Scanner(System.in);
+        String command = "";
+
+        while(!command.contains("stop")){
+            System.out.println("Введите команду или текст: ");
+            command = sc.nextLine();
         
+            if(command.contains("print")){
+                System.out.println(list.toString());
+            } else if(command.contains("remove")){
+                System.out.print("Введите индекс объекта для удаления: ");
+                int index = sc.nextInt();
+                list.remove(index);
+                System.out.println(list.toString());
+            } else {
+                list.add(command);
+            }
+        }
+
+        sc.close();
     }
 }
